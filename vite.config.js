a139f84f4a4933b3b1bdd466c12689d2dee5dccd
Fabio Henriques <fabiohvp@hotmail.com>
2026-07-20 23:34:37 -0300

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import fs from 'fs'
+
+try {
+  if (fs.existsSync('level-up.mp3')) {
+    fs.copyFileSync('level-up.mp3', 'public/level-up.mp3');
+  }
+  if (fs.existsSync('point-up.mp3')) {
+    fs.copyFileSync('point-up.mp3', 'public/point-up.mp3');
+  }
+} catch (err) {
+  console.error(err);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 })
+
 
